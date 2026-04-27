ce0dd6fe4eda815b4d71759d7ddfa9cd5ec6e231
RohithBN <rohithbn27@gmail.com>
2026-04-27 15:32:04 +0530

    add billing service and establish grpc comm inbetween

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,33 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+        log.info("Create billing account request received {}",request.toString());
+
+        //business logic
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+        // send response from grpc server to grpc client
+        responseObserver.onNext(response);
+        // tells response is completed
+        responseObserver.onCompleted();
+
+
+
+    }
+}
